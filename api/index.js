29771b16d4ad28c12df587403b5a744f21607eb4
Vitor Nunes <stecks10@gmail.com>
2021-08-14 12:46:42 -0300

    Implementando resolvers

--- api/index.js
+++ api/index.js
@@ -1,20 +1,11 @@
 const { ApolloServer } = require('apollo-server');
 const userSchema = require('./user/schema/user.graphql');
+const userResolvers = require('./user/resolvers/userRevolvers');
 
-const users = [
-  {
-    nome: 'Ana',
-    ativo: true,
-  },
-  {
-    nome: 'Marcia',
-    ativo: false,
-  },
-];
 //SDL =  scheme definition language
 
 const typeDefs = [userSchema];
-const resolvers = {};
+const resolvers = [userResolvers];
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
